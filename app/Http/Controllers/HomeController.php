e8da27d6d226cad5394f58d83a82b3a919d310c5
noahJ101 <37839884+noahJ101@users.noreply.github.com>
2024-01-30 06:46:46 +0100

    Commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,8 @@ use App\Models\Product;
 
 use App\Models\Cart;
 
+use App\Models\Order;
+
 class HomeController extends Controller
 {
     public function index() 
@@ -94,7 +96,93 @@ class HomeController extends Controller
     }
 
     public function show_cart()
-    {
-        return view('home.showcart');
+    {  
+        if(Auth::id())
+        {
+            $id=Auth::user()->id;
+
+            $cart=cart::where('user_id', '=', $id)->get();
+            return view('home.showcart',compact('cart'));
+        }
+
+        else
+        {
+            return redirect('login');
+        }
+       
     }
+
+    public function remove_cart($id)
+    {
+        $cart=cart::find($id);
+
+        $cart->delete();
+
+        return redirect()->back();
+    }
+
+    public function cash_order()
+
+    {
+        $user=Auth::user();
+
+        $userid=$user->id;
+
+        $data=cart::where('user_id', '=', $userid)->get();
+
+        foreach($data as $data)
+
+        {
+        
+            $order=new order;
+
+            $order->name=$data->name;
+
+            $order->email=$data->email;
+            
+            $order->phone=$data->phone;
+
+            $order->address=$data->address;
+
+            $order->user_id=$data->user_id;
+
+            $order->product_title=$data->product_title;
+
+            $order->price=$data->price;
+
+            $order->quantity=$data->quantity;
+
+            $order->image=$data->image;
+
+            $order->product_id=$data->product_id;
+
+            $order->payment_status='cash on delivery';
+
+            $order->delivery_status='processing';
+
+            $order->save();
+
+            $cart_id=$data->id;
+
+            $cart=cart::find($cart_id);
+
+            $cart->delete();
+
+
+
+            
+            
+        
+        
+
+        }
+
+    
+    
+
+        return redirect()-back();
+    
+    }
+
+
 }
