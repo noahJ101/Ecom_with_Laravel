00a8acb220543e4efc55d0fa3d5e6e9ca2e0a76f
James <noahjamesobekpa101@gmail.com>
2024-02-14 00:02:05 -0800

    New

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <!-- Basic Laravel -->
+      <!-- Basic Laravel  https://github.com/noahJ101/Ecom.git -->  
       <meta charset="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <!-- Mobile Metas -->
